fbf92c5e2afb1b84732e46282592e887fa5a9114
Paul Breaux <p.breaux@f5.com>
2016-04-15 15:18:54 -0600

    Allow custom startup script
    Issues: Issue #9
    
    Problem: The ve_image_sync needs to allow a custom startup script.
    
    Analysis: The patch-image tool now expects a tarball of the startup
    script or scripts. The ve_image_sync module can accept a custom startup
    package.
    
    Tests:

--- test/functional/test_ve_image_sync.py
+++ test/functional/test_ve_image_sync.py
@@ -13,10 +13,8 @@
 # limitations under the License.
 #
 
-from f5_image_prep.openstack.glance import GlanceLib
 from f5_image_prep.openstack.openstack import get_creds
 
-import os
 import pytest
 import subprocess
 import sys
@@ -25,36 +23,37 @@ import sys
 BIGIPFILE = 'BIGIP-11.6.0.0.0.401.qcow2'
 VEIS_SCRIPT = \
     '/home/imageprep/f5-openstack-image-prep/f5_image_prep/ve_image_sync.py'
-os.environ['OS_TENANT_NAME'] = 'admin'
-os.environ['OS_USERNAME'] = 'admin'
-os.environ['OS_PASSWORD'] = 'changeme'
-os.environ['OS_AUTH_URL'] = 'http://10.190.4.147:5000/v2.0'
+STARTUP_SCRIPT = \
+    '/home/imageprep/f5-openstack-image-prep/lib/f5_image_prep/startup.tar'
 TEST_IMG = None
 
 
 @pytest.fixture
-def VEImageSync(request):
+def VEImageSync(request, set_env_vars, glanceclientmanager):
     from f5_image_prep.ve_image_sync import VEImageSync as veis
+    set_env_vars
 
     def delete_image():
-        GlanceLib(get_creds()).glance_client.images.delete(TEST_IMG.id)
+        glanceclientmanager.images.delete(TEST_IMG.id)
 
     request.addfinalizer(delete_image)
 
     creds = get_creds()
     work_dir = sys.path[0]
-    return veis(creds, BIGIPFILE, work_dir)
+    return veis(creds, BIGIPFILE, STARTUP_SCRIPT, work_dir)
 
 
-def test_image_sync(VEImageSync):
+def test_image_sync(VEImageSync, glanceclientmanager):
     global TEST_IMG
     TEST_IMG = VEImageSync.sync_image()
-    imgs = GlanceLib(get_creds()).glance_client.images.list()
+    imgs = glanceclientmanager.images.list()
     assert TEST_IMG.id in [img.id for img in imgs]
 
 
 def test_image_sync_command_line():
-    output = subprocess.check_output(['python', VEIS_SCRIPT, '-i', BIGIPFILE])
+    output = subprocess.check_output(
+        ['python', VEIS_SCRIPT, '-i', BIGIPFILE, '-s', STARTUP_SCRIPT]
+    )
     assert 'Patching image...' in output
     assert 'Uploading patched image to glance...' in output
     assert 'Image Model:' in output
